a1dd8cc0404da74e4b1673b2056971c697c0798a
bdew <blackdew@gmail.com>
2015-01-04 10:49:45 +0200

    updated to bdlib 1.6.0

--- src/net/bdew/ae2stuff/machines/encoder/GuiEncoder.scala
+++ src/net/bdew/ae2stuff/machines/encoder/GuiEncoder.scala
@@ -18,7 +18,7 @@ class GuiEncoder(cont: ContainerEncoder) extends BaseScreen(cont, 176, 166) {
   override val background = Texture(AE2Stuff.modId, "textures/gui/encoder.png", rect)
   override def initGui() {
     super.initGui()
-    widgets.add(new WidgetLabel(BlockEncoder.getLocalizedName, 8, 6, Color.darkgray))
-    widgets.add(new WidgetLabel(Misc.toLocal("container.inventory"), 8, this.ySize - 96 + 3, Color.darkgray))
+    widgets.add(new WidgetLabel(BlockEncoder.getLocalizedName, 8, 6, Color.darkGray))
+    widgets.add(new WidgetLabel(Misc.toLocal("container.inventory"), 8, this.ySize - 96 + 3, Color.darkGray))
   }
 }
--- src/net/bdew/ae2stuff/machines/grower/GuiGrower.scala
+++ src/net/bdew/ae2stuff/machines/grower/GuiGrower.scala
@@ -18,7 +18,7 @@ class GuiGrower(cont: ContainerGrower) extends BaseScreen(cont, 176, 166) {
   override val background = Texture(AE2Stuff.modId, "textures/gui/grower.png", rect)
   override def initGui() {
     super.initGui()
-    widgets.add(new WidgetLabel(BlockGrower.getLocalizedName, 8, 6, Color.darkgray))
-    widgets.add(new WidgetLabel(Misc.toLocal("container.inventory"), 8, this.ySize - 96 + 3, Color.darkgray))
+    widgets.add(new WidgetLabel(BlockGrower.getLocalizedName, 8, 6, Color.darkGray))
+    widgets.add(new WidgetLabel(Misc.toLocal("container.inventory"), 8, this.ySize - 96 + 3, Color.darkGray))
   }
 }
