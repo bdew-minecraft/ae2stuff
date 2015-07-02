194cd4c4ccd49ac8fe343dc7a0114475f51463fd
bdew <blackdew@gmail.com>
2015-07-02 20:14:31 +0300

    Fixed slot wierdness in pattern encoder (#40)

--- src/net/bdew/ae2stuff/machines/encoder/slots.scala
+++ src/net/bdew/ae2stuff/machines/encoder/slots.scala
@@ -16,6 +16,7 @@ import net.minecraft.inventory.{IInventory, Slot}
 import net.minecraft.item.ItemStack
 
 class SlotFakeCrafting(inv: IInventory, slot: Int, x: Int, y: Int, onChanged: () => Unit) extends Slot(inv, slot, x, y) with SlotClickable {
+  override def canTakeStack(p: EntityPlayer) = false
   override def onClick(button: Int, mods: Int, player: EntityPlayer) = {
     val newStack = if (player.inventory.getItemStack != null) {
       val stackCopy = player.inventory.getItemStack.copy()
@@ -36,6 +37,7 @@ class SlotFakeCrafting(inv: IInventory, slot: Int, x: Int, y: Int, onChanged: ()
 
 class SlotFakeCraftingResult(inv: IInventory, slot: Int, x: Int, y: Int) extends Slot(inv, slot, x, y) {
   override def canTakeStack(p: EntityPlayer) = false
+  override def isItemValid(s: ItemStack) = false
 }
 
 class SlotFakeEncodedPattern(inv: TileEncoder, slot: Int, x: Int, y: Int) extends Slot(inv, slot, x, y) with SlotClickable {
